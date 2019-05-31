cb3b1648c9f44f8b641bde5556a086b19e8a4ce3
Gale <sgale@teamscci.local>
2019-05-31 14:51:25 -0500

    removeNote complete

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,66 @@
-const getNotes = require('./notes.js');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const msg = getNotes();
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(msg);
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'sting',
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note to remove',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title);
+    }
+});
+
+// List
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing all notes.');
+    }
+});
+
+// Read
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note.');
+    }
+});
+
+yargs.parse();
+// console.log(yargs.argv);
