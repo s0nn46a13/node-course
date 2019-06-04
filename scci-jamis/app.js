2c743ccd6070a1d8cbe52add523275128fc7cfb1
Sonny <sonny.gale@gmail.com>
2019-06-03 19:14:26 -0500

    email

--- scci-jamis/app.js
+++ scci-jamis/app.js
@@ -1,4 +1,7 @@
+
 const jamis = require('./jamis.js');
+
+
 
 // const buf1 = Buffer.from('phrase to obfuscate');
 // console.log(buf1.toString('hex'));
--- scci-jamis/jamis.js
+++ scci-jamis/jamis.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const request = require('request');
+const nodemailer = requie('nodemailer');
 
 const jamisURL = (Buffer.from('68747470733a2f2f54696d656d6f6e69746f723a507574757274316d33696e324441592140736363692e6a616d69737072696d652e636f6d2f6f646174612f534343492f54696d65436172644d6f6e69746f72696e6732', 'hex')).toString();;
 const getEmail = [];
