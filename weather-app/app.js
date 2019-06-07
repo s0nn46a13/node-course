4a0456c957270e7ed12188fb09249c9dd6c62def
Sonny <sonny.gale@gmail.com>
2019-06-07 17:17:45 -0500

    commit

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const geocode = require('./utils/geocode')
+
 // const url = 'https://api.darksky.net/forecast/8ac4f163892c8d2c6b2765a49e34de18/37.8267,-122.4233';
 
 // request({ url: url, json: true }, (error, response) => {
@@ -12,12 +13,22 @@ const geocode = require('./utils/geocode')
 //     }
 // });
 
-// Geocoding
-// Address -> lat/Long -> Weather
+// geocode('Boston', (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data',data)
+// });
 
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
 
-
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data',data)
-});
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+  });
