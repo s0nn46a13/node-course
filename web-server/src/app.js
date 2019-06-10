ecc9b283628bb3cfeb1aba4b29c11d5130a4e8f6
Sonny <sonny.gale@gmail.com>
2019-06-10 18:59:11 -0500

    lesson 48 complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,44 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.set('view engine','hbs')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Sonny Gale',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sonny Gale',
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('Help', {
+        helpText: 'This is some helpful text.',
+    });
+});
+
+//
+// Goal: Create a template for help page
+//
+// 1. Setup a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the route in the browser and see your help message print
 
 app.get('/weather', (req, res) => {
     res.send({
