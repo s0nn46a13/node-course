5f1823a46418e950adf1bf26e2cb88ef97589c0e
Sonny <sonny.gale@gmail.com>
2019-06-10 18:14:00 -0500

    lesson 47 halfway

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
