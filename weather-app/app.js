4504ed453856b629ae682950c2f4b8d95feb688f
Gale <sgale@teamscci.local>
2019-06-10 13:58:20 -0500

    lesson 39 complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,30 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const address = process.argv[2]
+//
+// Goal: Use both destructuring and property shorthand in weather app
+//
+// 1. Use destructuring in app.js, forecast.js, and geocode.js
+// 2. Use property shorthand in forecast.js and geocode.js
+// 3. Test your work and ensure app still works
+
+const address = process.argv[2];
 
 if (!address) {
   console.log('Please provide an address.')
 } else {
-  geocode(address, (error, data) => {
+  geocode(address, (error, { latitude, longitude, location }) => {
     if (error) {
       return console.log(error);
-    }
+    };
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return console.log(error);
-      }
+      };
 
-      console.log(data.location);
+      console.log(location);
       console.log(forecastData);
     });
   });
-}
+};
