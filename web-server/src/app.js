3817242ffd78b5cc8bd83deaea96652a597fabce
Sonny <sonny.gale@gmail.com>
2019-06-10 19:28:16 -0500

    lesson 49 complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
+//
+// Goal: Create a partial for the footer
+//
+// 1. Setup the template for the footer partial "Created by Some Name"
+// 2. Render the partial at the bottom of all three pages
+// 3. Test your work by visiting all three pages
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine','hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -31,6 +41,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('Help', {
         helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sonny Gale',
     });
 });
 
