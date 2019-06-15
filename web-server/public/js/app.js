7c16c1c5dd6953088e9900c2471a51097e66b471
Sonny <sonny.gale@gmail.com>
2019-06-15 06:28:10 -0500

    lesson 59 complete

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,26 @@ console.log('Client side javascript file is loaded!');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#msg-1');
+const messageTwo = document.querySelector('#msg-2');
+
+// messageOne.textContent = 'From Javascript';
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
 
+    messageOne.textContent = 'loading...';
+    messageTwo.textContent = '';
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                mesageOne.textContent = data.error;
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
             };
         });
     });
